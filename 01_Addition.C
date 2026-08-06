044e3c84345a987ed797b8e73d54e158b6020b7d
mhaskesnehal60-design <mhaskesnehal60@gmail.com>
2026-08-06 22:29:27 +0530

    Added 1st Addition program

---
+++ 01_Addition.C
@@ -0,0 +1,21 @@
+/* 
+program: WAP to find addition of two numbers
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a,b,c;
+	system("cls");
+	printf("\n\t Enter the value of a:");
+	scanf("%d",&a);
+	printf("\n\t) Enter the value of b:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("\n\t c= %d",c);
+	printf("\n\t Addition of %d and %d is %d",a,b,c);
+	printf("\n\t\" Addition of %d and %d is %d\"",a,b,c);
+	printf("\n\t He\bllo");
+	printf("\n\t Hello\a");
+	return 0;
+}
