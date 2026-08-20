7872690433f478559172fc23ed0ad3110a9f3cd4
Snehal Mhaske <mhaskesnehal60@gmail.com>
2026-08-20 21:52:10 +0530

    Add files via upload

---
+++ exchange no. 2 C.c
@@ -0,0 +1,15 @@
+/* program: WAP to exchange two number using 3rd variable
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	system("cls");
+	printf("\n\t Enter the value of a:");
+	scanf("%d, &a:");
+	printf("\n\t Enter the value of b:");
+	scanf("%d, &b:");
+    printf("\n\t  After exchange: a=%d b=%d,a,b");
+    return 0;
+	
+}
